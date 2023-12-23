6c353e6ff1073dba942cb5b09893f5e6aa64c467
IRACK (CHAEUN) <81044362+IRACK000@users.noreply.github.com>
2023-12-24 05:21:36 +0900

    Fix: Fix typo

--- main.py
+++ main.py
@@ -105,15 +105,13 @@ def read_files(request: Request, file_path: str, current_directory: Path = Depen
         )
     else:
         if file_path.endswith(".html"):
-            file_location = current_directory / file_path.replace(".html", "")
-            print(file_location)
             video_ext = [".mp4", ".webm", ".ogg"], [".mkv", ".avi", ".mov", ".wmv", ".ts"]  # html5, not html5
             for _type, ext_type in enumerate(video_ext):
-                for ext in ext_type:
-                    for extt in (ext, ext.upper()):
-                        candidate = file_location / extt
+                for extension in ext_type:
+                    for ext in (extension, extension.upper()):
+                        url = file_path.replace(".html", ext)
+                        candidate = current_directory / url
                         if candidate.is_file():
-                            url = file_path.replace(".html", extt)
                             return templates.TemplateResponse(
                                 f"player{'5' if _type == 0 else ''}.html",
                                 {"request": request, "file_name": candidate.name, "video_url": url},
